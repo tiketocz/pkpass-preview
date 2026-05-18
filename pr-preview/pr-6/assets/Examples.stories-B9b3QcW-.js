91b4077a055c07e5c3f0dc25ae6a512dc05ad399
jakubknejzlik <jakubknejzlik@users.noreply.github.com>
2026-05-18 15:13:58 +0000

    Deploy preview for PR 6 🛫

---
+++ pr-preview/pr-6/assets/Examples.stories-B9b3QcW-.js
@@ -0,0 +1,11 @@
+import{j as p}from"./jsx-runtime-BjG_zV1W.js";import{C as i}from"./Comparison-DuEHJU4f.js";import{e as l,a as d}from"./example-store-card-WCui8D59.js";import"./index-DoD0tp75.js";import"./index-BGsE_GxY.js";const f={title:"Examples",render:c=>p.jsx(i,{...c}),parameters:{layout:"centered"}},e={args:{values:l,screenshot:"example-generic"}},r={args:{values:d,screenshot:"example-store-card"}};var a,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    values: exampleGeneric,
+    screenshot: "example-generic"
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    values: exampleStoreCard,
+    screenshot: "example-store-card"
+  }
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const h=["Generic","StoreCard"];export{e as Generic,r as StoreCard,h as __namedExportsOrder,f as default};
