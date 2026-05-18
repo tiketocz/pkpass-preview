66e574348c719c5bec915faa48228b98bde4040d
jakubknejzlik <jakubknejzlik@users.noreply.github.com>
2026-05-18 16:18:06 +0000

    Deploy preview for PR 5 🛫

---
+++ pr-preview/pr-5/assets/BoardingPass.stories-MyS4y54N.js
@@ -0,0 +1,19 @@
+import{j as u}from"./jsx-runtime-BjG_zV1W.js";import{C as b}from"./Comparison-DuEHJU4f.js";import{b as B,a as l,c as P}from"./boarding-pass-3-DkyU69eA.js";import"./index-DoD0tp75.js";import"./index-BGsE_GxY.js";const S={title:"Boarding pass",render:m=>u.jsx(b,{...m}),parameters:{layout:"centered"}},s={name:"Boarding pass 1",args:{values:B,screenshot:"boarding-pass-1"}},a={name:"Boarding pass 2",args:{values:l,screenshot:"boarding-pass-2"}},r={name:"Boarding pass 3",args:{values:P,screenshot:"boarding-pass-3"}};var e,o,n;s.parameters={...s.parameters,docs:{...(e=s.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  name: "Boarding pass 1",
+  args: {
+    values: boardingPass1,
+    screenshot: "boarding-pass-1"
+  }
+}`,...(n=(o=s.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var d,i,t;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: "Boarding pass 2",
+  args: {
+    values: boardingPass2,
+    screenshot: "boarding-pass-2"
+  }
+}`,...(t=(i=a.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};var p,g,c;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: "Boarding pass 3",
+  args: {
+    values: boardingPass3,
+    screenshot: "boarding-pass-3"
+  }
+}`,...(c=(g=r.parameters)==null?void 0:g.docs)==null?void 0:c.source}}};const C=["BoardingPass1","BoardingPass2","BoardingPass3"];export{s as BoardingPass1,a as BoardingPass2,r as BoardingPass3,C as __namedExportsOrder,S as default};
