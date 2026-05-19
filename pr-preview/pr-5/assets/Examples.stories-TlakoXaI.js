499b9949d7d6b2e97c9c54e702e1f3cee780134b
jakubknejzlik <jakubknejzlik@users.noreply.github.com>
2026-05-19 08:25:03 +0000

    Deploy preview for PR 5 🛫

--- pr-preview/pr-5/assets/Examples.stories-TlakoXaI.js
+++ pr-preview/pr-5/assets/Examples.stories-TlakoXaI.js
@@ -1,4 +1,4 @@
-import{j as p}from"./jsx-runtime-BjG_zV1W.js";import{C as i}from"./Comparison-B4NStJ9t.js";import{e as l,a as d}from"./example-store-card-WCui8D59.js";import"./index-DoD0tp75.js";import"./index-BGsE_GxY.js";const f={title:"Examples",render:c=>p.jsx(i,{...c}),parameters:{layout:"centered"}},e={args:{values:l,screenshot:"example-generic"}},r={args:{values:d,screenshot:"example-store-card"}};var a,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+import{j as p}from"./jsx-runtime-BjG_zV1W.js";import{C as i}from"./Comparison-DUGQPh_9.js";import{e as l,a as d}from"./example-store-card-WCui8D59.js";import"./index-DoD0tp75.js";import"./index-BGsE_GxY.js";const f={title:"Examples",render:c=>p.jsx(i,{...c}),parameters:{layout:"centered"}},e={args:{values:l,screenshot:"example-generic"}},r={args:{values:d,screenshot:"example-store-card"}};var a,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
   args: {
     values: exampleGeneric,
     screenshot: "example-generic"
