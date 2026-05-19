a61240e7409c1e5b0c175889e5b7b57e7ecc85af
jakubknejzlik <jakub.knejzlik@gmail.com>
2026-05-19 10:18:28 +0000

    Deploying to gh-pages from @ tiketocz/pkpass-preview@834b10b7be34518182151c2ec178a81faa23a63b 🚀

---
+++ assets/BackFields.stories-DsOS2EbB.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-BjG_zV1W.js";import{C as m}from"./Comparison-DUGQPh_9.js";import{b as c}from"./back-fields-CzUse35s.js";import"./index-DoD0tp75.js";import"./index-BGsE_GxY.js";import"./_placeholder-Di2y90HQ.js";const k={title:"Back fields",render:o=>t.jsx(m,{...o}),parameters:{layout:"centered"}},e={name:"Back fields demo",args:{values:c,screenshot:"back-fields-1"}};var s,r,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: "Back fields demo",
+  args: {
+    values: backFields1,
+    screenshot: "back-fields-1"
+  }
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const u=["BackFields1"];export{e as BackFields1,u as __namedExportsOrder,k as default};
--- assets/preview-CVQ-K4cQ.js
+++
@@ -1,1 +0,0 @@
-const e={parameters:{layout:"centered",controls:{expanded:!0}}};export{e as default};
